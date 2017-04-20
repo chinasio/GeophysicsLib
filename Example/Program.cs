df46ab93e8e9fc76d5922ab7ceecedc8b7c37a80
chinasio <yangchguo@gmail.com>
2017-04-20 20:11:53 +0800

    建立git仓库

---
+++ Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeophysicsLib;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fv = IGRFMode.getF(2014, 1, 1, 11.5, 108.5, 1500);
+            Console.WriteLine(fv);
+        }
+    }
+}
